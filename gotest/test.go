9abf8e15d0101f7329e7a03609578e6065c0d8b6
魏小伟 <1550518923@qq.com>
2020-07-19 22:45:31 +0800

    go 语言

---
+++ gotest/test.go
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: V 1.0
+ * @Author: wxw
+ * @Date: 2020-07-19 10:48:04
+ */ 
+ package main  // 将test.go归属到main下
+ import "fmt"  // 引入一个包fmt
+ func main(){
+   fmt.Println("Hello World!")
+ }
