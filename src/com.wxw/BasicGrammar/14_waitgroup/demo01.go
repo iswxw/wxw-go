af3171a58629de78f1f99224ba0b17758cb50325
weixiaowei <1550518923@qq.com>
2021-10-25 00:21:00 +0800

    go waitGroup concurrent control

---
+++ src/com.wxw/BasicGrammar/14_waitgroup/demo01.go
@@ -0,0 +1,23 @@
+/*
+@Time: 2021/10/25 0:11
+@Author: wxw
+@File: demo01
+*/
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(-100)
+	for i := 1; i <= 100; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
