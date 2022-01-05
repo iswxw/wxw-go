cc06d1136e13ea4ea5b5d64a88a0e32a20ec3553
weixiaowei <weixiaowei@didichuxing.com>
2022-01-05 10:53:37 +0800

    json decode and encode

--- src/com.wxw/advance_grammar/w_json/demo_json.go
+++ src/com.wxw/advance_grammar/w_json/demo_json.go
@@ -6,6 +6,62 @@
 */
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type Student struct {
+	ID     int
+	Name   string
+	Sex    string
+	Course []string
+}
+
+func main() {
+	fmt.Println("==================== 开始 ==================")
+	// 编码
+	EncodeStruct()
+
+	// 分隔符
+	fmt.Println("==================== 分隔符 ==================")
+
+	// 解码
+	DecodeJsonString()
+	fmt.Println("==================== 结束 ==================")
+}
+
+/**
+ * 将一个对象编码成json字符串
+ */
+func EncodeStruct() {
+	student := Student{
+		ID:     14343466,
+		Name:   "Java半颗糖",
+		Sex:    "M",
+		Course: []string{"English", "Math", "Chinese"},
+	}
+	b, err := json.Marshal(student)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println(string(b))
+}
+
+/**
+ * 将json字符串解码成json对象
+ */
+func DecodeJsonString() {
+	var jsonBody = []byte(`{"ID":14343466,"Name":"Java半颗糖","Sex":"M","Course":["English","Math","Chinese"]}`)
+	var student1 = Student{}
+	err := json.Unmarshal(jsonBody, &student1)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(student1)
+	fmt.Println("studentName:", student1.Name)
+	fmt.Println("studentId:", student1.ID)
+	fmt.Println("studentSex:", student1.Sex)
+	fmt.Println("studentCourse:", student1.Course)
 }
---
+++ src/com.wxw/advance_grammar/w_json/demo_xml_to_json.go
@@ -0,0 +1,10 @@
+/*
+@Time : 2022/1/5 10:45
+@Author : wxw
+@File : demo_xml_to_json
+*/
+package main
+
+func main() {
+
+}
