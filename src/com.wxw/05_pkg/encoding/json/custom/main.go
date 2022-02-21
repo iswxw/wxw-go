10a0f89b7f6d2e6fbea975906d22265ef4b6204e
weixiaowei <weixiaowei@didichuxing.com>
2022-02-22 00:18:49 +0800

    [add] json source

---
+++ src/com.wxw/05_pkg/encoding/json/custom/main.go
@@ -0,0 +1,13 @@
+/*
+@Time : 2022/2/22 00:17
+@Author : weixiaowei
+@File : main
+*/
+package main
+
+import "fmt"
+
+// 自定义json序列化类型  https://www.jianshu.com/p/6015912a120d
+func main() {
+	fmt.Println(false)
+}
