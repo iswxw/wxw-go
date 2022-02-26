4fd44077bfa9586083e0b4d03e52fb5e51e29aee
weixiaowei <weixiaowei@didichuxing.com>
2022-02-26 21:10:37 +0800

    [add] 状态模式

---
+++ src/com.wxw/07_design_pattern/04_state_pattern/main.go
@@ -0,0 +1,11 @@
+/*
+@Time : 2022/2/26 17:52
+@Author : weixiaowei
+@File : main
+*/
+package main
+
+// 状态模式
+func main() {
+
+}
---
+++ src/com.wxw/07_design_pattern/main.go
@@ -0,0 +1,11 @@
+/*
+@Time : 2022/2/26 12:30
+@Author : weixiaowei
+@File : main
+*/
+package main
+
+func main() {
+
+	//cipher.StreamWriter{}
+}
