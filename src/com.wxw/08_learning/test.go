67de89f92aef9a5f1f8c8f2a7c5d3a3ccf83762b
weixiaowei <weixiaowei@didichuxing.com>
2022-05-11 17:45:52 +0800

    [update] test

--- src/com.wxw/08_learning/test.go
+++ src/com.wxw/08_learning/test.go
@@ -7,9 +7,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"github.com/spf13/cast"
 )
 
 func main() {
-	fmt.Println(time.Now().Unix())
+	m := map[string]interface{}{
+		"2": 2,
+	}
+
+	valueStr := m["2"]
+	fmt.Println(valueStr)
+
+	value := valueStr.(int)
+
+	fmt.Println(cast.ToInt(valueStr))
+	fmt.Println(value)
 }
