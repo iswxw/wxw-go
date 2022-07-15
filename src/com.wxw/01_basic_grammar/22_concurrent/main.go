b0ec1874c1c03aa7a195c7d9cd687076a08dd713
weixiaowei <1550518923@qq.com>
2022-07-15 23:13:06 +0800

    [add] 内存模型

---
+++ src/com.wxw/01_basic_grammar/22_concurrent/main.go
@@ -0,0 +1,23 @@
+/*
+@Time: 2022/7/10 23:04
+@Author: wxw
+@File: main
+*/
+package main
+
+var a, b int
+
+func f() {
+	a = 1 // w之前的写操作
+	b = 2 // 写操作w
+}
+
+func g() {
+	print(b) // 读操作r
+	print(a) // ???
+}
+
+func main() {
+	go f() //g1
+	g()    //g2
+}
