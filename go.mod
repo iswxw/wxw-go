0d6816c650778cdcb7b51e15ef5d9d8a76f0c5df
weixiaowei <1550518923@qq.com>
2022-08-15 23:40:17 +0800

    [add] go pdf

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dlclark/regexp2 v1.7.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jung-kurt/gofpdf v1.16.2 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -18,6 +19,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/signintech/gopdf v0.14.1 // indirect
+	github.com/tiechui1994/gopdf v0.0.0-20220723131034-4b11a58b280a // indirect
+	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
 	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20220512140231-539c8e751b99 // indirect
---
+++ src/com.wxw/03_thirdparty/w10_pdf/gopdf/demo02_generate_pdf.go
@@ -0,0 +1,99 @@
+/*
+@Time: 2022/8/15 23:25
+@Author: wxw
+@File: demo02_generate_pdf
+*/
+package main
+
+import (
+	"fmt"
+	"github.com/tiechui1994/gopdf"
+	"github.com/tiechui1994/gopdf/core"
+	"math/rand"
+	"time"
+)
+
+const (
+	TableIg = "IPAexG"
+	TableMd = "MPBOLD"
+	TableMy = "微软雅黑"
+)
+
+var (
+	seed = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
+func main() {
+
+	basePath := "D:\\Project\\wxw-go\\src\\com.wxw\\03_thirdparty\\w10_pdf\\"
+	r := core.CreateReport()
+	font1 := core.FontMap{
+		FontName: TableIg,
+		FileName: basePath + "common/ttf/Loma.ttf",
+	}
+	font2 := core.FontMap{
+		FontName: TableMd,
+		FileName: basePath + "common/ttf/Loma.ttf",
+	}
+	font3 := core.FontMap{
+		FontName: TableMy,
+		FileName: basePath + "common/ttf/Loma.ttf",
+	}
+	r.SetFonts([]*core.FontMap{&font1, &font2, &font3})
+	r.SetPage("A4", "P")
+
+	r.RegisterExecutor(core.Executor(SimpleTableExecutor), core.Detail)
+
+	r.Execute(basePath + "tmp/simple_table.pdf")
+	fmt.Println(r.GetCurrentPageNo())
+}
+
+func SimpleTableExecutor(report *core.Report) {
+	lineSpace := 1.0
+	lineHeight := 18.0
+
+	table := gopdf.NewTable(5, 100, 415, lineHeight, report)
+	table.SetMargin(core.Scope{})
+
+	// 先把当前的行设置完毕, 然后才能添加单元格内容.
+	c00 := table.NewCellByRange(1, 1)
+	c01 := table.NewCellByRange(2, 1)
+	c03 := table.NewCellByRange(2, 2)
+	c10 := table.NewCellByRange(3, 1)
+
+	f1 := core.Font{Family: TableMy, Size: 15, Style: ""}
+	border := core.NewScope(4.0, 4.0, 4.0, 3.0)
+	c00.SetElement(gopdf.NewTextCell(table.GetColWidth(0, 0), lineHeight, lineSpace, report).SetFont(f1).SetBorder(border).HorizontalCentered().SetContent("0-0"))
+	c01.SetElement(gopdf.NewTextCell(table.GetColWidth(0, 1), lineHeight, lineSpace, report).SetFont(f1).SetBorder(border).RightAlign().SetContent("0-1"))
+	c03.SetElement(gopdf.NewTextCell(table.GetColWidth(0, 3), lineHeight, lineSpace, report).SetFont(f1).SetBorder(border).RightAlign().SetContent("0-3近日，江苏苏州市姑苏区市场监督管理局向苏州定园旅游服务有限公司送达行政处罚决定书，对定园进行处罚，吊销其营业执照，此举开创了我国旅游景点因虚假宣传被吊销营业执照的先河"))
+	c10.SetElement(gopdf.NewTextCell(table.GetColWidth(1, 0), lineHeight, lineSpace, report).SetFont(f1).SetBorder(border).VerticalCentered().SetContent("1-0近日，江苏苏州市姑苏区市场监督管理局向苏州定园旅游服务有限公司送达行政处罚决定书了我国旅游景点因虚假宣传被吊销营业执照的先河"))
+
+	f1 = core.Font{Family: TableMy, Size: 10}
+	border = core.NewScope(4.0, 4.0, 0, 0)
+
+	for i := 0; i < 98; i++ {
+		cells := make([]*gopdf.TableCell, 5)
+		for j := 0; j < 5; j++ {
+			cells[j] = table.NewCell()
+		}
+
+		for j := 0; j < 5; j++ {
+			str := `有限公司送达行政处罚决定书`
+			s := fmt.Sprintf("%v-%v", i+2, str)
+			w := table.GetColWidth(i+2, j)
+			e := gopdf.NewTextCell(w, lineHeight, lineSpace, report)
+			e.SetFont(f1)
+			if i%2 == 0 {
+				e.SetBackColor("255,192,203")
+			}
+			e.SetBorder(border)
+			e.SetContent(s)
+			cells[j].SetElement(e)
+		}
+	}
+
+	if err := table.GenerateAtomicCell(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
