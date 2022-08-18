95a82690ee82f3d10d1c5b577f03745939389bca
weixiaowei <weixiaowei@didichuxing.com>
2022-08-18 14:21:41 +0800

    [update] write excel

--- src/com.wxw/03_thirdparty/w05_excel/integration/integration.go
+++ src/com.wxw/03_thirdparty/w05_excel/integration/integration.go
@@ -6,6 +6,9 @@ package integration
 
 import (
 	"context"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"log"
+	"src/com.wxw/project_actual/src/com.wxw/03_thirdparty/w05_excel/common/consts"
 	"src/com.wxw/project_actual/src/com.wxw/03_thirdparty/w05_excel/common/dto"
 )
 
@@ -15,25 +18,37 @@ type IExcelService interface {
 }
 
 func (e excelService) WriteToXlsxFile(ctx context.Context, param *dto.ParamWriteExcel) ([]byte, error) {
-	//
-	//f := excelize.NewFile()
-	//index := f.NewSheet(consts.SheetName)
-	//
-	////  设置表头
-	//f.SetSheetRow(consts.SheetName, "A1", &param.Headers)
-	//
-	//// 设置列宽
-	//for key, value := range param.ColumnWidths {
-	//	f.SetColWidth(consts.SheetName, key, key, value)
-	//}
-	//
-	//
 
-	return nil, nil
+	f := excelize.NewFile()
+
+	index := f.NewSheet(consts.SheetName)
+
+	//  设置表头
+	f.SetSheetRow(consts.SheetName, "A1", &param.Headers)
+
+	// 设置列宽
+	for key, value := range param.ColumnWidths {
+		f.SetColWidth(consts.SheetName, key, key, value)
+	}
+
+	// 设置工作簿的默认工作表
+	f.SetActiveSheet(index)
+
+	// TODO 记录日志
+	log.Println("param = ", param)
+
+	// 数据写入文件缓存
+	writeToBuffer, err := f.WriteToBuffer()
+	if err != nil {
+		return nil, err
+	}
+
+	return writeToBuffer.Bytes(), nil
 }
 
 func (e excelService) ReadFromXlsxFile(ctx context.Context, param *dto.ParamWriteExcel) ([]byte, error) {
-	panic("implement me")
+
+	return nil, nil
 }
 
 // 内部结构
