2da144db910cf7a42d6e33f38e1ffcd2048d9521
weixiaowei <weixiaowei@didichuxing.com>
2022-09-14 11:24:38 +0800

    [update] fix

---
+++ src/com.wxw/01_basic_grammar/02_basic_data_type/05_string_type/demo02_len.go
@@ -0,0 +1,8 @@
+// @Time : 2022/9/14 11:16
+// @Author : xiaoweiwei
+// @File : demo02_len
+
+package main
+
+func main() {
+}
--- src/com.wxw/08_learning/test.go
+++ src/com.wxw/08_learning/test.go
@@ -5,21 +5,6 @@
 */
 package main
 
-import (
-	"fmt"
-	"github.com/spf13/cast"
-)
-
 func main() {
-	m := map[string]interface{}{
-		"2": 2,
-	}
 
-	valueStr := m["2"]
-	fmt.Println(valueStr)
-
-	value := valueStr.(int)
-
-	fmt.Println(cast.ToInt(valueStr))
-	fmt.Println(value)
 }
