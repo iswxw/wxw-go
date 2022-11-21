a624ffdb60f083838941024bc0cc46fdf8b35515
weixiaowei <weixiaowei@didichuxing.com>
2022-11-21 23:32:59 +0800

    [update] 获取周的逻辑

--- src/com.wxw/02_advance_grammar/w_time/demo/demo04_week.go
+++ src/com.wxw/02_advance_grammar/w_time/demo/demo04_week.go
@@ -12,16 +12,54 @@ import (
 func main() {
 
 	now := time.Now()
-	week := now.Weekday()
-	offset := int(time.Monday - week)
-	if offset > 0 {
-		offset = -6
-	}
-	fmt.Println(offset)
+	currentMonday := GetMondayOfWeek(now, "2006-01-02")
+	lastMonday, err := GetLastWeekMonday(now, "2006-01-02")
+	lastSunday, err := GetLastWeekSunday(now, "2006-01-02")
+	fmt.Println(err)
+	fmt.Println(currentMonday)
+	fmt.Println(lastMonday)
+	fmt.Println(lastSunday)
+}
 
-	//周
-	StartDate := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
-	EndDates := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 999999999, time.Local)
-	fmt.Println("周一", StartDate.AddDate(0, 0, offset))            //周一
-	fmt.Println("周末", EndDates.AddDate(0, 0, int((week+1)-week))) //周末
+//获取本周周一的日期
+func GetMondayOfWeek(t time.Time, fmtStr string) (dayStr string) {
+	dayObj := GetZeroTime(t)
+	if t.Weekday() == time.Monday {
+		//修改hour、min、sec = 0后格式化
+		dayStr = dayObj.Format(fmtStr)
+	} else {
+		offset := int(time.Monday - t.Weekday())
+		if offset > 0 {
+			offset = -6
+		}
+		dayStr = dayObj.AddDate(0, 0, offset).Format(fmtStr)
+	}
+	return
+}
+
+//获取上周周一日期
+func GetLastWeekMonday(t time.Time, fmtStr string) (day string, err error) {
+	monday := GetMondayOfWeek(t, fmtStr)
+	dayObj, err := time.Parse(fmtStr, monday)
+	if err != nil {
+		return
+	}
+	day = dayObj.AddDate(0, 0, -7).Format(fmtStr)
+	return
+}
+
+//获取上周周日日期
+func GetLastWeekSunday(t time.Time, fmtStr string) (day string, err error) {
+	monday := GetMondayOfWeek(t, fmtStr)
+	dayObj, err := time.Parse(fmtStr, monday)
+	if err != nil {
+		return
+	}
+	day = dayObj.AddDate(0, 0, -1).Format(fmtStr)
+	return
+}
+
+//获取某一天的0点时间
+func GetZeroTime(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
---
+++ src/com.wxw/02_advance_grammar/w_time/demo/demo05_duration.go
@@ -0,0 +1,9 @@
+// @Time : 2022/11/15 16:39
+// @Author : xiaoweiwei
+// @File : demo05_duration
+
+package main
+
+func main() {
+
+}
