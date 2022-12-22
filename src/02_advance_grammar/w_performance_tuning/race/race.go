4270271530c6db052ca16bf8a6ee8bc048170945
weixiaowei <1550518923@qq.com>
2022-12-22 14:51:28 +0800

    race

--- src/02_advance_grammar/w_performance_tuning/race/race.go
+++ src/02_advance_grammar/w_performance_tuning/race/race.go
@@ -3,4 +3,22 @@
 @Author: wxw
 @File: race
 */
-package race
+package main
+
+import "time"
+
+func main() {
+	var x int
+	go func() {
+		for {
+			x = 1
+		}
+	}()
+
+	go func() {
+		for {
+			x = 2
+		}
+	}()
+	time.Sleep(10 * time.Second)
+}
