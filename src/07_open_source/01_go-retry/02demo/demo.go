85e14d1609d0f3a0f635a28675d29012cad915fb
weixiaowei <weixiaowei@didichuxing.com>
2023-02-17 17:46:10 +0800

    位运算符

---
+++ src/07_open_source/01_go-retry/02demo/demo.go
@@ -0,0 +1,13 @@
+// @Time : 2023/2/17 17:43
+// @Author : xiaoweiwei
+// @File : demo
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 << 0) // 1
+	fmt.Println(1 << 1) // 2
+	fmt.Println(1 << 2) // 4
+}
