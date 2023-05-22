7519939a27b3e36ac7551883c2c882e48bc80b01
weixiaowei <weixiaowei@didichuxing.com>
2023-05-22 21:00:53 +0800

    时间校验

---
+++ src/07_open_source/03_validator/demo/validator_time_test.go
@@ -0,0 +1,38 @@
+// @Time : 2023/5/22 20:50
+// @Author : xiaoweiwei
+// @File : validator_time
+
+package demo
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"testing"
+)
+
+type User struct {
+	Name     string `validate:"min=6,max=10"`
+	Age      int    `validate:"min=1,max=100"`
+	Birthday string `validate:"required,datetime=2006-01-02 15:04:05"`
+}
+
+// 出自：https://blog.51cto.com/u_15301988/5133385
+func TestCheckTime(t *testing.T) {
+	validate := validator.New()
+
+	u1 := User{
+		Name:     "wxw-go",
+		Age:      18,
+		Birthday: "2022-03-29 22:22:10",
+	}
+	err := validate.Struct(u1)
+	fmt.Println("u1 = ", err)
+
+	u2 := User{
+		Name:     "wxw-go",
+		Age:      18,
+		Birthday: "2022-03-29",
+	}
+	err1 := validate.Struct(u2)
+	fmt.Println("u2 = ", err1)
+}
