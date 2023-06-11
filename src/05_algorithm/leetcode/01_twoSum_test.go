d45eaf29b1b22ed6a1c01c82ac72ae1542962d07
weixiaowei <1550518923@qq.com>
2023-06-11 09:55:36 +0800

    两数之和

--- src/05_algorithm/leetcode/01_twoSum_test.go
+++ src/05_algorithm/leetcode/01_twoSum_test.go
@@ -2,21 +2,34 @@
 @Time : 2022/1/11 12:52
 @Author : wxw
 @File : 01_twoSum
-@Link : https://e.leetcode-cn.com/enterprise/joclmpkwdowf/preview/question/1/
+@Link : https://leetcode.cn/problems/two-sum/
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 // 题目描述： 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
-func main() {
+
+func TestTwoSum(t *testing.T) {
 	// 定义一个数组
 	nums := []int{1, 2, 3, 4}
 	arrayIndex := twoSum(nums, 4)
 	fmt.Println("arrayIndex = ", arrayIndex)
 }
 
-func twoSum(nums []int, target int) []int {
+// 方法一：暴力遍历  O(n2)
+// 方法二：hash分析 O（n）
 
-	return nums
+func twoSum(nums []int, target int) []int {
+	hashMap := make(map[int]int, 0)
+	for i, num := range nums {
+		if v, ok := hashMap[target-num]; ok {
+			return []int{v, i}
+		}
+		hashMap[num] = i
+	}
+	return nil
 }
