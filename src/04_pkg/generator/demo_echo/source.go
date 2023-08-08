4c4bd706378e810974aa12982d6917c78860716b
weixiaowei <1550518923@qq.com>
2023-08-08 22:25:55 +0800

    generator 工具

---
+++ src/04_pkg/generator/demo_echo/source.go
@@ -0,0 +1,11 @@
+/*
+@Time: 2023/8/8 22:10
+@Author: wxw
+@File: source
+*/
+package demo_echo
+
+// 参考案例：https://blog.csdn.net/qq_41893274/article/details/132176534
+
+//go:generate echo "Hello, World!"
+//go:generate echo "Hello, World!"
