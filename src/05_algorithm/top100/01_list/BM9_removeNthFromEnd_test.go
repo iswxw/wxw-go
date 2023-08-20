28bd28e02aa93cf96ca7a7a1c54848377e8f2474
weixiaowei <1550518923@qq.com>
2023-08-20 17:54:58 +0800

    upd

---
+++ src/05_algorithm/top100/01_list/BM9_removeNthFromEnd_test.go
@@ -0,0 +1,15 @@
+/*
+@Time: 2023/8/20 17:53
+@Author: wxw
+@File: BM9_removeNthFromEnd_test 给定一个链表，删除链表的倒数第 n 个节点并返回链表的头指针
+*/
+package _1_list
+
+import "src/com.wxw/project_actual/src/05_algorithm/dto"
+
+// 双指针法
+func removeNthFromEnd(head *dto.ListNode, n int) *dto.ListNode {
+	// write code here
+
+	return nil
+}
