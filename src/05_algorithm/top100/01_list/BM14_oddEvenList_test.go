63882457376caaced5c31aaaaa8e4d90b17a628a
weixiaowei <1550518923@qq.com>
2023-09-02 22:28:28 +0800

    upd 链表奇偶重排后输出

---
+++ src/05_algorithm/top100/01_list/BM14_oddEvenList_test.go
@@ -0,0 +1,14 @@
+/*
+@Time: 2023/9/2 22:26
+@Author: wxw
+@File: BM14_oddEvenList_test
+*/
+
+// 给定一个单链表，请设定一个函数，将链表的奇数位节点和偶数位节点分别放在一起，重排后输出。
+package _1_list
+
+import "testing"
+
+func TestBM14(t *testing.T) {
+
+}
