266e2de7a0799c7f100b3960ab5efd96f8e0f692
weixiaowei <1550518923@qq.com>
2023-09-10 23:35:56 +0800

    upd 数组中的逆序对

---
+++ src/05_algorithm/top100/02_query_sort/BM20_InversePairs_test.go
@@ -0,0 +1,28 @@
+/*
+@Time: 2023/9/10 23:24
+@Author: wxw
+@File: BM20_InversePairs_test
+*/
+package main
+
+import "testing"
+
+// 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组,求出这个数组中的逆序对的总数P
+//
+func TestBM20(t *testing.T) {
+
+}
+
+// 方法一：归并排序 NLogN
+func InversePairs(nums []int) int {
+	// write code here
+
+	return 0
+}
+
+// 方法二：暴力破解 O(n2)
+func InversePairs01(nums []int) int {
+	// write code here
+
+	return 0
+}
