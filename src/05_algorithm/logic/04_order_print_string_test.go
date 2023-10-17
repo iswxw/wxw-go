3b9193d23a2a7761fdb9c6e62c492001e3da2926
weixiaowei <weixiaowei@didichuxing.com>
2023-10-17 21:03:26 +0800

    upd

---
+++ src/05_algorithm/logic/04_order_print_string_test.go
@@ -0,0 +1,23 @@
+// @Time : 2023/10/17 19:05
+// @Author : xiaoweiwei
+// @File : 04_order_print_string_test
+
+package logic
+
+import "testing"
+
+// 分别打印按顺序fish dog cat 100次
+
+func Test04(t *testing.T) {
+
+}
+
+func printFish() {
+
+}
+
+/**
+参考材料
+  1.使用协程，按顺序打印cat、dog、fish各100次：https://blog.csdn.net/qq_37102984/article/details/121791579
+
+*/
---
+++ src/05_algorithm/logic/05_limit_goroutine_test.go
@@ -0,0 +1,37 @@
+// @Time : 2023/10/17 20:09
+// @Author : xiaoweiwei
+// @File : 05_limit_goroutine_test
+
+package logic
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// limitChan 限制协程个数
+func Test05(t *testing.T) {
+	limitChan := make(chan struct{}, 10) // 最大协程数限制为10个
+	for i := 0; i < 100; i++ {
+		go job(i, limitChan)
+	}
+
+}
+
+// 任务函数
+func job(i int, limitChan chan struct{}) {
+	limitChan <- struct{}{}
+	defer func() {
+		<-limitChan
+	}()
+
+	time.Sleep(1 * time.Second)
+	fmt.Printf("任务:%d已完成，当前协程数:%d\n", i, len(limitChan))
+}
+
+/**
+参考材料
+  1.限制协成个数：https://blog.csdn.net/qq_37102984/article/details/121791579
+
+*/
